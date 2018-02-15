ea2cdda97aba37e4c7f14963f3b830635584a853
Federico Soave <fede.covr@gmail.com>
2018-02-15 22:53:23 +0100

    drop express, rewrite http server for browser test

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,39 +1,13 @@
-var fs = require('fs'),
-    path = require('path');
+var http = require('http'),
+    fs = require('fs');
 
-var testMod = require('../'),
-    load = testMod.load;
-
-var express = require('express'),
-    app = express();
-
-var files = load();
-
-app.use(function(req, res, next) {
-  var setHeader = res.setHeader;
-  res.setHeader = function(name) {
-    switch (name) {
-      case 'Cache-Control':
-      case 'Last-Modified':
-      case 'ETag':
-        return;
-    }
-    return setHeader.apply(res, arguments);
-  };
-  next();
-});
-
-app.get('/test.js', function(req, res, next) {
-  var test = fs.readFileSync(path.join(__dirname, 'test.js'), 'utf8');
-  var testScript = test.replace('__TESTS__', JSON.stringify(files))
-    .replace('__MAIN__', testMod.runTests + '')
-    .replace('__LIBS__', testMod.testFile + '');
-
-  res.contentType('.js');
-  res.send(testScript);
-});
-
-app.use(express.static(path.join(__dirname, '/../../lib')))  ;
-app.use(express.static(__dirname));
-
-app.listen(8080);
+http.createServer(function(req, res) {
+  try {
+    var file = fs.readFileSync(__dirname + req.url);
+    res.writeHead(200);
+    res.end(file);
+  } catch (e) {
+    res.writeHead(404);
+    res.end();
+  }
+}).listen(8080);
