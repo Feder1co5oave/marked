8c370fd054ee618f544e64b897fda10c9c09ffde
Federico Soave <fede.covr@gmail.com>
2018-02-22 23:52:11 +0100

    pass json string to test.load() to bypass loading from compiled_tests.json

--- test/index.js
+++ test/index.js
@@ -27,7 +27,9 @@ function load(options) {
       files,
       glob = g2r(options.glob || '*', { extended: true });
 
-  files = JSON.parse(fs.readFileSync(compiled, 'utf8'));
+  files = options.json ?
+    JSON.parse(options.json) :
+    JSON.parse(fs.readFileSync(compiled, 'utf8'));
 
   if (options.glob) {
     files = files.filter(function(test) {
